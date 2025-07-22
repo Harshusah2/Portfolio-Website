b270389b78e88e70d1cbf4f43236b62fba5c901b
Harsh Sahu <111741242+Harshusah2@users.noreply.github.com>
2025-07-23 00:00:21 +0530

    Make changes in the About section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -41,7 +41,7 @@ export const AboutSection = () => {
                             </a>
 
                             <a 
-                                href="CV/Harsh_RESUME.pdf" 
+                                href="CV/resume_harsh_sahu.pdf" 
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
